03d609501ce412d154ff2cd1e1e0b4ce1e81256c
tarioto <timarioto@gmail.com>
2016-10-13 19:20:03 -0700

    refactoring for mongoose

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,32 @@
 var db = require('../config');
-var bcrypt = require('bcrypt-nodejs');
+var bcrypt = require('bcrypt-nodejs'); 
 var Promise = require('bluebird');
+var mongoose = require('mongoose');  
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function() {
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+var userSchema =mongoose.Schema ({
+  username: { type: String, index: {unique: true} },
+  password: String
+};
+
+var User = mongoose.model('User', userSchema);
+
+User.prototype.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    if (err) {
+      callback(err);
+    } else {
       callback(isMatch);
+    }
+  });
+};
+
+userSchema.pre('save', function(next) {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password), null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash);
+      next();
     });
-  },
-  hashPassword: function() {
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
 });
 
 module.exports = User;
